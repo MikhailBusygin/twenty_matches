72353a17bbeae5cdc720f48b0279c3b4f53afdb1
Mikhail Busygin <mix_ice@mail.ru>
2020-12-23 06:23:06 +0400

    game create

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Game game = new Game();
+        Computer computer = new Computer();
+        Console console = new Console();
+        console.startGame();
+        do {
+            console.printGameValue(game.getGameValue());
+            game.setValue(console.getComputerValue(computer.getComputerValue(game.getGameValue())));
+            if (game.getGameValue() > 1) {
+                console.printGameValue(game.getGameValue());
+                game.setValue(console.getPlayerValue());
+            } else {
+                break;
+            }
+        } while (game.getGameValue() > 0);
+        console.endGame();
     }
 }
